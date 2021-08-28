b4f83e9f8f7020a581923c642f77b9984c2b5a81
Chkuaseli <62430415+Chkuaseli@users.noreply.github.com>
2021-08-29 01:34:56 +0400

    Update pdfkit_conf.py

--- customer/card/pdfkit_conf.py
+++ customer/card/pdfkit_conf.py
@@ -3,10 +3,10 @@ import pdfkit
 
 if platform.system() == 'Windows':
     pdfkit_config = pdfkit.configuration(wkhtmltopdf=os.environ.get('WKHTMLTOPDF_PATH','C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'))
-if 'DYNO' in os.environ:
-    print ('loading wkhtmltopdf path on heroku')
-    WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf-pack')], stdout=subprocess.PIPE).communicate()[0].strip()
+# if 'DYNO' in os.environ:
+#     print ('loading wkhtmltopdf path on heroku')
+#     WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf-pack')], stdout=subprocess.PIPE).communicate()[0].strip()
 else:
-    WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_PATH', 'wkhtmltopdf')],stdout=subprocess.PIPE).communicate()[0].strip()
+    WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf-pack')],stdout=subprocess.PIPE).communicate()[0].strip()
     pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
 
