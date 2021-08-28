21c3b63311a188b4b73af92dbf01ff94cd8b9cfc
Chkuaseli <62430415+Chkuaseli@users.noreply.github.com>
2021-08-28 13:12:30 +0400

    Update __init__.py

--- customer/__init__.py
+++ customer/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from .card.models import db 
 from .config import Config
-from flask_uploads import IMAGES, UploadSet, configure_uploads, patch_request_class
+from flask_uploads import IMAGES, UploadSet, configure_uploads
 from flask_msearch import Search
 
 app = Flask(__name__)
@@ -22,7 +22,7 @@ Migrate(app, db)
 #flask uploads
 photos = UploadSet('photos', IMAGES)
 configure_uploads(app, photos)
-patch_request_class(app)
+
 
 
 from customer.card import route
