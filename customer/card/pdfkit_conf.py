255b3e955837c38beb88968313f3f5b00063cc1c
Chkuaseli <62430415+Chkuaseli@users.noreply.github.com>
2021-08-28 17:20:16 +0400

    Update pdfkit_conf.py

--- customer/card/pdfkit_conf.py
+++ customer/card/pdfkit_conf.py
@@ -5,6 +5,6 @@ if platform.system() == 'Windows':
     pdfkit_config = pdfkit.configuration(wkhtmltopdf=os.environ.get('WKHTMLTOPDF_PATH','C:\\Program Files\\wkhtmltopdf\\bin\\wkhtmltopdf.exe'))
     
 else:
-    WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_PATH', '/app/bin/wkhtmltopdf')],stdout=subprocess.PIPE).communicate()[0].strip()
+    WKHTMLTOPDF_CMD = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_PATH', '/usr/local/bin/wkhtmltopdf')],stdout=subprocess.PIPE).communicate()[0].strip()
     pdfkit_config = pdfkit.configuration(wkhtmltopdf=WKHTMLTOPDF_CMD)
 
