088938bbe0d204622ab764e5f515c1b2335e2539
Chkuaseli <62430415+Chkuaseli@users.noreply.github.com>
2021-08-28 14:20:25 +0400

    Update config.py

--- customer/config.py
+++ customer/config.py
@@ -5,7 +5,7 @@ class Config:
     DEBUG = True
     TESTING = True
 #   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
-    SQLALCHEMY_DATABASE_URI = 'postgres://akpisqqwdpsnyz:039341e558ac05f36f2b43ce5cfd2abed4f00c0782fead8317bbe44994a268a7@ec2-54-73-147-133.eu-west-1.compute.amazonaws.com:5432/ddk08pv3hmslrf'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://akpisqqwdpsnyz:039341e558ac05f36f2b43ce5cfd2abed4f00c0782fead8317bbe44994a268a7@ec2-54-73-147-133.eu-west-1.compute.amazonaws.com:5432/ddk08pv3hmslrf'
     SQLALCHEMY_TRACK_MODIFICATIONS =False
     SECRET_KEY = 'swisscapital' 
 #     # photos 
